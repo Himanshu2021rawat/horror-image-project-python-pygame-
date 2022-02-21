a73fa1a58feaf27e922873df710256c3f9749231
himanshu <himanshurawat25691@gmail.com>
2022-02-21 10:29:14 +0530

    Initial Comit

---
+++ flaskproject.py
@@ -0,0 +1,16 @@
+# flask module mai se flask class define kari
+from flask import Flask,render_template
+
+app = Flask(__name__,template_folder='template')
+
+@app.route("/")
+def hello_world():
+        return render_template('index.html')
+
+@app.route("/about")
+def hello():
+    name = "himanshu"
+
+    return render_template('about.html',name=name)
+
+app.run(debug=True)
